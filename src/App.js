ac5910b74110ba93a5a1d63dbc3a5baf10a376b6
Jonas Mazarini de Oliveira <jonasmazarini@hotmail.com>
2021-03-22 16:53:55 -0300

    Atualização de arquivos

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
 import React from "react";
-import { Provider } from "react-redux";
 import Router from "./routes";
-import store from "./store";
 import "./reset.css";
 import "./App.css";
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Router />
-    </Provider>
-  );
+  return <Router />;
 }
 
 export default App;
